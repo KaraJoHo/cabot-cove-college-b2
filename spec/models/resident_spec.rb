d10682f152c438528fc458d0ec1c0588ba5e9826
KaraJH <karas.jones@yahoo.com>
2023-02-09 18:21:31 -0700

    test: Average age in resident model

--- spec/models/resident_spec.rb
+++ spec/models/resident_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Resident, type: :model do
   let!(:jessica) {Resident.create!(name: "Jessica Fletcher", age: 65, occupation: "Mystery Writer")} 
+  let!(:seth) {Resident.create!(name: "Seth Guy", age: 70, occupation: "Retired")}
   let!(:course_1) {Course.create!(name: "Crime Scenes 101")}
   let!(:course_2) {Course.create!(name: "Fingerprinting")}
   let!(:rescourse_1) {ResidentCourse.create!(resident_id: jessica.id, course_id: course_1.id)}
@@ -21,6 +22,12 @@ RSpec.describe Resident, type: :model do
   describe '#courses_list' do 
     it 'has all the courses of the resident' do 
       expect(jessica.courses_list).to eq([course_1, course_2])
+    end
+  end
+
+  describe '::average_age' do 
+    it 'is the average age of all residents' do 
+     expect(Resident.average_age).to eq(67.5)
     end
   end
 
