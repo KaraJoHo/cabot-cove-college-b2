59226a21e72ee506ec72b7dc311a522301d9bc80
KaraJH <karas.jones@yahoo.com>
2023-02-09 18:27:54 -0700

    feat: Average age in resident index. completed 4/4 user stories

--- app/controllers/residents_controller.rb
+++ app/controllers/residents_controller.rb
@@ -1,11 +1,13 @@
 class ResidentsController < ApplicationController 
   def index 
     @residents = Resident.all
+    @avg_age = Resident.average_age
   end
 
   def show 
     @resident = Resident.find(params[:id])
     @resident_courses = @resident.courses_list
+
   end
 
 end
